cd7dab17b3fdcd8a9cf8dc42d05d031a200d2aaa
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2022-09-04 21:42:34 +0800

    Build simple stack classes.

---
+++ WiredBrainCoffee.StackApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace WiredBrainCoffee.StackApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackDoubles();
+            StackStrings();
+            Console.ReadLine();
+        }
+        private static void StackDoubles()
+        {
+            var stack = new SimpleStackDouble();
+            stack.Push(1.2);
+            stack.Push(2.8);
+            stack.Push(3.0);
+
+            double sum = 0.0;
+
+            while (stack.Count > 0)
+            {
+                double item = (double)stack.Pop();
+                Console.WriteLine($"Item: {item}");
+                sum += item;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+        private static void StackStrings()
+        {
+            var stack = new SimpleStackString();
+            stack.Push("Wired Brain Coffee");
+            stack.Push("Pluralsight");
+
+            while(stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
---
+++ WiredBrainCoffee.StackApp/SimpleStack.cs
@@ -0,0 +1,24 @@
+﻿namespace WiredBrainCoffee.StackApp
+{
+    public class SimpleStackDouble
+    {
+        private readonly double[] _item;
+        private int _currentIndex = -1;
+
+        public SimpleStackDouble() => _item = new double[10];
+        public int Count => _currentIndex + 1;
+        public void Push(double item) => _item[++_currentIndex] = item;
+        public double Pop() => _item[_currentIndex--];
+    }
+
+    public class SimpleStackString
+    {
+        private readonly string[] _item;
+        private int _currentIndex = -1;
+
+        public SimpleStackString() => _item = new string[10];
+        public int Count => _currentIndex + 1;
+        public void Push(string item) => _item[++_currentIndex] = item;
+        public string Pop() => _item[_currentIndex--];
+    }
+}
